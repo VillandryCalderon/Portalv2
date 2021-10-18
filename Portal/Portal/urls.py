a0a030be72ebcd48d36efd30a2a4632dac9bd652
Villandry <calderon_102@hotmail.com>
2021-10-17 23:12:04 -0600

    second_commit

---
+++ Portal/Portal/urls.py
@@ -0,0 +1,57 @@
+"""Portal URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from jobs.views import home
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from django import urls
+
+from django.conf.urls import url, include
+from jobs.routers import DefaultRouter
+from education.urls import router as router_education
+from companies.urls import router as router_companies
+from infoprofile.forms import FormularioUser
+from infoprofile.urls import router as router_user_profile
+from django.contrib.auth import views as auth_views
+from infoprofile.views import formularioUserView
+
+
+router = DefaultRouter()
+router.extend(router_education)
+router.extend(router_companies)
+router.extend(router_user_profile)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('jobs/', include('jobs.urls', namespace='jobs')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('',include('jobs.urls')),
+    
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
