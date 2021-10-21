f882e5d2647690a200a1c0f177d6f17dfd519947
Villandry <calderon_102@hotmail.com>
2021-10-21 17:24:08 -0600

    Se arreglo el job add

--- Portal/jobs/models.py
+++ Portal/jobs/models.py
@@ -38,21 +38,20 @@ GENDER = (
 
 
 class JobListing(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-                             null=True, editable=False, blank=True)
-    title = models.CharField(max_length=100)
-    company_name = models.CharField(max_length=200)
-    employment_status = models.CharField(choices=JOB_TYPE, max_length=10)
-    vacancy = models.CharField(max_length=10, null=True)
-    gender = models.CharField(choices=GENDER, max_length=30, null=True)
-    category = models.CharField(choices=CATEGORY, max_length=30)
-    description = models.TextField()
-    responsibilities = models.TextField()
-    experience = models.CharField(max_length=100)
-    job_location = models.CharField(max_length=120)
-    Salary = models.CharField(max_length=100, null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True, editable=False, blank=True)
+    title = models.CharField(max_length=100, verbose_name='Titulo del Puesto')
+    company_name = models.CharField(max_length=200,default="Istmo Center",verbose_name="Compañia")
+    employment_status = models.CharField(choices=JOB_TYPE, max_length=10, verbose_name="Tipo de contrato")
+    vacancy = models.CharField(max_length=10, null=True,verbose_name="Vacantes")
+    gender = models.CharField(choices=GENDER, max_length=30, null=True, verbose_name="Genero")
+    category = models.CharField(choices=CATEGORY, max_length=30,verbose_name="Categoria")
+    description = models.TextField(verbose_name="Descripción")
+    responsibilities = models.TextField(verbose_name="responsabilidades")
+    experience = models.CharField(max_length=100,verbose_name="Experiencia")
+    job_location = models.CharField(max_length=120,verbose_name="Provincia")
+    Salary = models.CharField(max_length=100, null=True, blank=True,verbose_name="Salario")
     image = models.ImageField(blank=True, upload_to='media', null=True)
-    application_deadline = models.DateTimeField()
+    application_deadline = models.DateTimeField(verbose_name="Fecha limite")
     published_on = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
